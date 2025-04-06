1750b5724df4f95d3a3a370b40ce2db34f96c50d
Roei Rubach <rrubach@gmail.com>
2025-04-06 14:24:12 +0300

    Update StateMachine.cs

--- Assets/_Project/Code/Systems/StateMachine/StateMachine.cs
+++ Assets/_Project/Code/Systems/StateMachine/StateMachine.cs
@@ -1,58 +1,94 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Trivainia
 {
     public class StateMachine
     {
-        private readonly HashSet<IState> _activeStates = new();
-        private readonly List<Transition> _transitions = new();
+        private StateNode _currentNode;
+        private readonly List<Transition> _globalTransitions = new();
         private readonly Dictionary<Type, StateNode> _nodes = new();
 
         public void Update()
         {
-            var transition = GetTransition();
+            var transition = ProcessTransitions();
             if (transition != null)
                 ChangeState(transition.To);
 
-            foreach (var state in _activeStates)
-                state?.Update();
+            _currentNode.State?.Update();
         }
 
-        public void FixedUpdate()
-        {
-            foreach (var state in _activeStates)
-                state?.FixedUpdate();
-        }
+        public void FixedUpdate() => _currentNode.State?.FixedUpdate();
 
         public void SetState(IState state)
         {
-            currentNode = nodes[state.GetType()];
-            currentNode.State?.OnEnter();
+            _currentNode = _nodes[state.GetType()];
+            _currentNode.State?.OnEnter();
         }
 
-        public void AddGlobalTransition(IState to, IPredicateStrategy condition) { }
+        public void AddGlobalTransition(IState to, IPredicateStrategy condition) => _globalTransitions.Add(new Transition(GetOrAddNode(to).State, condition));
+        public void AddTransition(IState from, IState to, IPredicateStrategy condition) => GetOrAddNode(from).AddTransition(GetOrAddNode(to).State, condition);
 
-        public void AddTransition(IState from, IState to, IPredicateStrategy condition) { }
+        private Transition ProcessTransitions()
+        {
+            foreach (var transition in _globalTransitions.Where(transition => transition.Condition.Evaluate()))
+                return transition;
 
-        private void ProcessTransitions() { }
+            return _currentNode.Transitions.FirstOrDefault(transition => transition.Condition.Evaluate());
+        }
 
         private void ChangeState(IState state)
         {
-            if (state == currentNode.State)
+            if (IsTryingToChangeToSameState(state))
                 return;
 
-            var previousState = currentNode.State;
-            var nextState = nodes[state.GetType()].State;
+            var previousActiveState = _currentNode;
+            var nextState = _nodes[state.GetType()].State;
 
-            previousState?.OnExit();
+            previousActiveState.State?.OnExit();
             nextState.OnEnter();
-            currentNode = nodes[state.GetType()];
+            _currentNode = _nodes[state.GetType()];
+        }
+
+        private bool IsTryingToChangeToSameState(IState state)
+        {
+            if (!_currentNode.State.IsComposite)
+                return _currentNode.State == state;
+
+            return CompareDeepestStatesInComposite(_currentNode.State, state);
+        }
+
+        private static bool CompareDeepestStatesInComposite(IState rootState, IState targetState)
+        {
+            var compositeState = rootState as ICompositeState;
+            var subStates = compositeState.SubStates;
+
+            foreach (var subState in subStates)
+                if (subState.IsComposite)
+                {
+                    if (CompareDeepestStatesInComposite(subState, targetState))
+                        return true;
+                }
+                else if (subState == targetState)
+                {
+                    return true;
+                }
+
+            return false;
         }
 
         private StateNode GetOrAddNode(IState state)
         {
-            
+            var node = _nodes.GetValueOrDefault(state.GetType());
+
+            if (node == null)
+            {
+                node = new StateNode(state);
+                _nodes[state.GetType()] = node;
+            }
+
+            return node;
         }
 
         private class StateNode

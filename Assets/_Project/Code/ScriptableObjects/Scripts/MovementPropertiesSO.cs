42f6fa8dd7cf6ea9fc959e35a6aa3d15a3272f16
Roei Rubach <rrubach@gmail.com>
2025-04-07 18:44:57 +0300

    Created SO assembly and simple movement properties

--- Assets/_Project/Code/ScriptableObjects/Scripts/MovementPropertiesSO.cs
+++ Assets/_Project/Code/ScriptableObjects/Scripts/MovementPropertiesSO.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Trivainia
@@ -5,13 +6,20 @@ namespace Trivainia
     [CreateAssetMenu(fileName = "MovementPropertiesSO", menuName = "Scriptable Objects/MovementPropertiesSO")]
     public class MovementPropertiesSO : ScriptableObject
     {
-        [Header("Speed Settings")]
+        [BoxGroup("Speed Settings")]
+        [LabelText("Movement Speed")]
         public float MoveSpeed = 5f;
         //public float SprintSpeed = 8f;
+        
+        [BoxGroup("Speed Settings")]
+        [LabelText("Rotation Speed")]
         public float RotationSpeed = 5f;
         
-        [Header("Acceleration Settings")]
+        [BoxGroup("Acceleration Settings")]
+        [LabelText("Acceleration")]
         public float Acceleration = 10f;
+        [BoxGroup("Acceleration Settings")]
+        [LabelText("Deceleration")]
         public float Deceleration = 10f;
     }
 }

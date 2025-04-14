e839a371ebdff5177bb52340b614264729b1b5bc
Roei Rubach <rrubach@gmail.com>
2025-04-14 17:48:12 +0300

    Update AbilityController.cs

--- Assets/_Project/Code/UnityIntegration/Ability/AbilityController.cs
+++ Assets/_Project/Code/UnityIntegration/Ability/AbilityController.cs
@@ -1,56 +1,60 @@
-﻿using System.Collections.Generic;
-using ImprovedTimers;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine.EventSystems;
 
 namespace Trivainia
 {
     public class AbilityController
     {
-        private readonly AbilityView _view;
         private readonly AbilityModel _model;
-        private readonly CountdownTimer _timer = new(0);
-        private readonly Queue<AbilityCommand> _abilityQueue = new();
+        private readonly AbilityView _view;
+        private readonly AbilityQueue _queue;
+        private readonly AbilityCooldownManager _cooldown;
 
         private AbilityController(AbilityView view, AbilityModel model)
         {
             _view = view;
             _model = model;
+            _queue = new AbilityQueue();
+            _cooldown = new AbilityCooldownManager();
 
             ConnectModel();
             ConnectView();
         }
 
-        private void ConnectModel() => _model.AbilityAdded += UpdateButtons;
+        private void ConnectModel() => _model.AbilityAdded += _view.UpdateButtonSprites;
 
         private void ConnectView()
         {
+            _cooldown.ProgressChanged += _view.UpdateRadial;
+
             foreach (var button in _view.Buttons)
                 button.RegisterListener(OnAbilityButtonPressed);
 
             _view.UpdateButtonSprites(_model.Abilities);
         }
 
-        public void Update()
-        {
-            _timer.Tick();
-            _view.UpdateRadial(_timer.Progress);
-
-            if (!_timer.IsRunning && _abilityQueue.TryDequeue(out var cmd))
-            {
-                cmd.Execute();
-                _timer.Reset(cmd.Duration);
-                _timer.Start();
-            }
-        }
-
-        private void UpdateButtons(List<Ability> updatedAbilities) => _view.UpdateButtonSprites(updatedAbilities);
-
         private void OnAbilityButtonPressed(int index)
         {
-            if (_timer.Progress < 0.25f || !_timer.IsRunning)
-                if (_model.Abilities[index] != null)
-                    _abilityQueue.Enqueue(_model.Abilities[index].CreateCommand());
             EventSystem.current.SetSelectedGameObject(null);
+
+            if (!_model.TryGetAbility(index, out var ability))
+                return;
+
+            if (_cooldown.CanQueue(ability.Data.QueueThreshold))
+                _queue.Enqueue(ability.CreateCommand());
+
+            if (!_cooldown.IsRunning)
+                ExecuteNextAsync().Forget();
+        }
+
+        private async UniTaskVoid ExecuteNextAsync()
+        {
+            if (!_queue.TryDequeue(out var cmd))
+                return;
+
+            cmd.Execute();
+            await _cooldown.RunCooldown(cmd.Duration);
+            ExecuteNextAsync().Forget();
         }
 
         public class Builder
@@ -65,10 +69,7 @@ namespace Trivainia
                 return this;
             }
 
-            public AbilityController Build(AbilityView view)
-            {
-                return new AbilityController(view, _model);
-            }
+            public AbilityController Build(AbilityView view) => new(view, _model);
         }
     }
 }

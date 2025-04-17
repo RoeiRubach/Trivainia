1c6167eb8ba535ae8a0ac615be4ed115a6763b86
Roei Rubach <rrubach@gmail.com>
2025-04-17 16:00:01 +0300

    Add: Implement dynamic input icons using ScriptableObjects
    - Created `AbilitiesInputImagesSO` to store directional input icons in clockwise order.
    - Added `PlayerInputSpritesLocator` MonoBehaviour to bridge SO data and UI systems.
    - Added asset references for controller and keyboard input icons via:
      - `KeyboardImages.asset`
      - `ControllerImages.asset`
      - `SkillBottom.asset`
    - Enabled prefab-driven button icon switching based on input type.

---
+++ Assets/_Project/Code/Gameplay/Player/MonoBehaviours/PlayerInputSpritesLocator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Trivainia
+{
+    public class PlayerInputSpritesLocator : MonoBehaviour
+    {
+        [SerializeField, Required] private AbilitiesInputImagesSO _keyboardInputSprites;
+        [SerializeField, Required] private AbilitiesInputImagesSO _controllerInputSprites;
+        
+        public IEnumerable<Sprite> GetSpritesClockwiseFromBottom(InputDeviceType deviceType)
+        {
+            return deviceType switch
+                   {
+                       InputDeviceType.KeyboardMouse   => _keyboardInputSprites.GetImagesClockwiseFromBottom(),
+                       InputDeviceType.Gamepad => _controllerInputSprites.GetImagesClockwiseFromBottom(),
+                       _                    => throw new System.ArgumentOutOfRangeException(nameof(deviceType), deviceType, null)
+                   };
+        }
+    }
+}
---
+++ Assets/_Project/Code/ScriptableObjects/Scripts/AbilitiesInputImagesSO.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Trivainia
+{
+    [CreateAssetMenu(fileName = "AbilitiesInputImagesSO", menuName = "Scriptable Objects/AbilitiesInputImagesSO")]
+    public class AbilitiesInputImagesSO : ScriptableObject
+    {
+        [Title("Clockwise Input Images (Starting from Bottom)")]
+    
+        [PreviewField(70), LabelText("Bottom")]
+        [PropertySpace(SpaceBefore = 5, SpaceAfter = 5)]
+        public Sprite SkillBottom;
+
+        [PreviewField(70), LabelText("Left")]
+        [PropertySpace(SpaceBefore = 5, SpaceAfter = 5)]
+        public Sprite SkillLeft;
+
+        [PreviewField(70), LabelText("Up")]
+        [PropertySpace(SpaceBefore = 5, SpaceAfter = 5)]
+        public Sprite SkillUp;
+
+        [PreviewField(70), LabelText("Right")]
+        [PropertySpace(SpaceBefore = 5, SpaceAfter = 10)]
+        public Sprite SkillRight;
+
+        public IEnumerable<Sprite> GetImagesClockwiseFromBottom()
+        {
+            yield return SkillBottom;
+            yield return SkillLeft;
+            yield return SkillUp;
+            yield return SkillRight;
+        }
+    }
+}

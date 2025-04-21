69c0d0c8d3673328ccda52c093fa01a7c0201944
Roei Rubach <rrubach@gmail.com>
2025-04-21 19:51:53 +0300

    Add: enemy dummy with health UI

---
+++ Assets/_Project/Code/Gameplay/Common/MonoBehaviours/AlwaysFaceMainCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Trivainia
+{
+    public class AlwaysFaceMainCamera : MonoBehaviour
+    {
+        private Transform _mainCamera;
+
+        private IEnumerator Start()
+        {
+            _mainCamera = FindAnyObjectByType<PlayerServiceLocator>().MainCamera;
+
+            while (true)
+            {
+                var direction = transform.position - _mainCamera.transform.position;
+                transform.rotation = Quaternion.LookRotation(direction);
+
+                yield return null;
+            }
+        }
+    }
+}
